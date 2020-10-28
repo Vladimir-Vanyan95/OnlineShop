f9e9f467e22188e71f7deb2d3e42655327a97b72
Khoren Antonyan <khorenantonian@gmail.com>
2020-10-28 23:16:06 +0300

    Add Category Repository Interface

---
+++ Data/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        void Add(CategoryViewModel categoryViewModel);
+        List<CategoryViewModel> GetAll();
+        void Delete(int id);
+    }
+}
