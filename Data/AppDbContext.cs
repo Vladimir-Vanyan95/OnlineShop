5ae1bff16d71e2931e4decbbd386aac033ef182f
Khoren Antonyan <khorenantonian@gmail.com>
2020-10-29 18:00:29 +0300

    Add Azure Database

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -15,5 +15,6 @@ namespace Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
     }
 }
---
+++ Data/Models/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+    }
+}
